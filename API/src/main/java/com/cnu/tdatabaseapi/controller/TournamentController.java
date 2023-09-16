1f8fc24d1b12fc88ccfe5f08199f6d3ad118445d
Alex Peters <alexap9982@gmail.com>
2023-09-16 02:27:48 -0400

    Shortened API uri

--- API/src/main/java/com/cnu/tdatabaseapi/controller/TournamentController.java
+++ API/src/main/java/com/cnu/tdatabaseapi/controller/TournamentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/database")
+@RequestMapping("/api")
 public class TournamentController {
     @Autowired
     private TournamentService tournamentService;
